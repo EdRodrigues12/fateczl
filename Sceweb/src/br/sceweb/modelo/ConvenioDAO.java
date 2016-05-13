a6281673f5397006842528f7000ca794ba9e755c
EdRodrigues12 <edirodrigues97@gmail.com>
2016-05-13 16:18:50 -0300

    4° Versao

--- Sceweb/src/br/sceweb/modelo/ConvenioDAO.java
+++ Sceweb/src/br/sceweb/modelo/ConvenioDAO.java
@@ -12,6 +12,13 @@ import br.sceweb.servico.FabricaDeConexoes;
 public class ConvenioDAO {
 	
 	Logger logger = Logger.getLogger(ConvenioDAO.class);
+	
+	/**
+	 * 
+	 * @param convenio
+	 * @return codigoRetorno se o convenio for inserido com sucesso no mysql
+	 */
+	
 	public int adiciona(Convenio convenio){
 	PreparedStatement ps;
 	int codigoRetorno=0;
@@ -29,5 +36,28 @@ public class ConvenioDAO {
 	}
 	return codigoRetorno;
 
+	
+
 }
+	/**
+	 * 
+	 * @param cnpj
+	 * @return codigoRetorno se o convenio for excluido com sucesso no mysql
+	 */
+	
+	public int excluir (String cnpj) {
+		java.sql.PreparedStatement ps;
+		int codigoretorno = 0;
+		try (Connection conn = (Connection) new FabricaDeConexoes().getConnection()) {
+			ps= conn.prepareStatement ("delete from convenio where empresa_cnpj = ?");
+			ps.setString(1, cnpj);
+			codigoretorno = ps.executeUpdate();
+			}
+		catch (SQLException e){
+			throw new RuntimeException(e);
+		}
+		return codigoretorno;
+	
+	}
+	
  }
--- Sceweb/src/br/sceweb/teste/UC05CadastrarConvenio.java
+++ Sceweb/src/br/sceweb/teste/UC05CadastrarConvenio.java
@@ -2,6 +2,7 @@ package br.sceweb.teste;
 
 import static org.junit.Assert.*;
 
+import org.joda.time.DateTime;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -33,14 +34,57 @@ public class UC05CadastrarConvenio {
 	convenio = new Convenio("81965361000174","03/05/2016", "20/05/2016");
 	}
 	
+	
+	
 	@Test
 	public void CT01UC05CadastrarConvenio_com_sucesso() {
 	assertEquals(1,convenioDAO.adiciona(convenio));
 	
 	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void CT06UC05A3CadastrarConvenio_cnpj_invalido() {
+		cnpj = "11111";
+		convenio.setCnpj(cnpj);
+	}
+	@Test
+	public void CT04UC05A2Cadastrar_convenio_dti_invalida(){
+		assertFalse(convenio.validaData("42/05/2016"));
+	}
+//	
+//	@Test(expected=IllegalArgumentException.class)
+//	public void CT03UC05A2Cadastrar_convenio_dti_invalida(){
+//		convenio.setDataInicio("42/05/2016");
+//	}
+	
+//	@Test(expected=IllegalArgumentException.class)
+//	public void CT03UC05A2Cadastrar_convenio_dtt_invalida(){
+//		convenio.setDataTermino("42/05/2016");
+//	}
+	
+	
+//	@Test
+//	public void CT02UC05A1Cadastrar_convenio_ja_cadastrado(){
+//		DateTime di = new DateTime(2016, 4, 5, 0, 0);
+//		DateTime df = new DateTime(2016, 5, 1, 0, 0);
+//		
+//		
+//		assertEquals (4, convenio.convenioJaCadastrado(df,di));
+//	}
+	
+
+	
+	@Test
+	public void CT06UC05CadastrarConvenio_cnpj_nao_cadastrado() {
+	Convenio novoConvenio = new Convenio("95702253000000","05/05/2016","05/12/2016");
+	assertEquals(0,convenioDAO.adiciona(novoConvenio));
+	
+	}
 
 	@AfterClass
 	public static void tearDownAfterClass() throws Exception {
+		convenioDAO.excluir("81965361000174");
+		empresaDAO.excluir("81965361000174");
 		
 	}
 
